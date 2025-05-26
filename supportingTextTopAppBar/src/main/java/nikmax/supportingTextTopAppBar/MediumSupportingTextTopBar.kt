dd83af4915049fafc6c486320ee91a5d96bda7a2
nikmax <null@null.null>
2025-05-26 21:35:00 +0300

    Modified preview

--- supportingTextTopAppBar/src/main/java/nikmax/supportingTextTopAppBar/MediumSupportingTextTopBar.kt
+++ supportingTextTopAppBar/src/main/java/nikmax/supportingTextTopAppBar/MediumSupportingTextTopBar.kt
@@ -12,6 +12,9 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -103,11 +106,22 @@ private fun Preview() {
     Scaffold(
         topBar = {
             MediumSupportingTextTopBar(
-                title = { Text("Title") },
-                supportingText = { Text("Supporting text") },
+                title = { Text("Title text") },
+                supportingText = { Text("Some supporting text") },
                 navigationIcon = {
                     IconButton(onClick = { }) {
                         Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = null)
+                    }
+                },
+                actions = {
+                    IconButton(onClick = { }) {
+                        Icon(Icons.Default.Favorite, contentDescription = null)
+                    }
+                    IconButton(onClick = { }) {
+                        Icon(Icons.Default.Search, contentDescription = null)
+                    }
+                    IconButton(onClick = { }) {
+                        Icon(Icons.Default.Delete, contentDescription = null)
                     }
                 },
                 scrollBehavior = scrollBehavior
